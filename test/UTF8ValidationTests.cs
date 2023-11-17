448a02e6fff671705c3ba5c80862e00e07515f94
Nick Nuon <myjpntrip@gmail.com>
2023-11-17 11:42:19 -0500

    Comments

--- test/UTF8ValidationTests.cs
+++ test/UTF8ValidationTests.cs
@@ -261,33 +261,53 @@ public void TestBadSequences()
         }
     }
 
+    // We save this for when testing SIMD version, I promise to clean up later:
+    // [Fact]
+    // public void BruteForceTest()
+    // {
+    //     for (int i = 0; i < NumTrials; i++)
+    //     {
+    //         byte[] utf8 = generator.Generate(rand.Next(256));
+    //         Assert.True(ValidateUtf8(utf8), "UTF-8 validation failed, indicating a bug.");
 
+    //         for (int flip = 0; flip < 1000; flip++)
+    //         {
+    //             byte[] modifiedUtf8 = (byte[])utf8.Clone();
+    //             int byteIndex = rand.Next(modifiedUtf8.Length);
+    //             int bitFlip = 1 << rand.Next(8);
+    //             modifiedUtf8[byteIndex] ^= (byte)bitFlip;
 
-    [Fact]
-    public void BruteForceTest()
-    {
-        uint seed = 1234;
-        RandomUtf8 gen_1_2_3_4 = new RandomUtf8(seed, 1, 1, 1, 1); // Assuming this class is implemented
-        Random rand = new Random();
-        int total = 1000;
+    //             bool isValid = ValidateUtf8(modifiedUtf8);
+    //             // This condition may depend on the specific behavior of your validation method
+    //             // and whether or not it should match a reference implementation.
+    //             // In this example, we are simply asserting that the modified sequence is still valid.
+    //             Assert.True(isValid, "Mismatch in UTF-8 validation detected, indicating a bug.");
+    //         }
+    //     }
+    // }
 
-        for (int i = 0; i < total; i++)
-        {
-            byte[] utf8 = gen_1_2_3_4.Generate(rand.Next(256));
-            Assert.True(ValidateUtf8(utf8));
+    // Pseudocode for easier ChatGPT generatioN:
+    // 1. Set a seed value (1234).
+    // 2. Create a random UTF-8 generator with equal probabilities for 1, 2, 3, and 4-byte sequences.
+    // 3. Set the total number of trials to 1000.
 
-            // for (int flip = 0; flip < 1000; flip++)
-            // {
-            //     // Flip exactly one bit in a random byte
-            //     int byteIndex = rand.Next(utf8.Length);
-            //     int bitPosition = rand.Next(8);
-            //     utf8[byteIndex] ^= (byte)(1 << bitPosition);
+    // 4. For each trial (0 to total - 1):
+    //    a. Generate a random UTF-8 sequence with a length between 0 and 255.
+    //    b. Validate the UTF-8 sequence. If it's invalid:
+    //       - Output "bug" to stderr.
+    //       - Fail the test.
 
-            //     // Re-validate the UTF-8 bytes after the bit flip
-            //     Assert.True(ValidateUtf8(utf8));
-            // }
-        }
-    }
+    //    c. For 1000 times (bit flipping loop):
+    //       i. Generate a random bit position (0 to 7).
+    //       ii. Flip exactly one bit at the random position in a random byte of the UTF-8 sequence.
+    //       iii. Re-validate the modified UTF-8 sequence.
+    //       iv. Compare the result of the validation with a reference validation method.
+    //       v. If the results differ:
+    //          - Output "bug" to stderr.
+    //          - Fail the test.
+
+    // 5. If all tests pass, output "OK".
+
 
     private bool ValidateUtf8(byte[] utf8)
     {
